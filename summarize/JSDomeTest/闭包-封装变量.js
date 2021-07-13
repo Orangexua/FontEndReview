8798ea29162ea94c969921290661d5f31d01f1d2
OrangeXu <1249858341@qq.com>
2021-07-13 10:29:24 +0800

    单例模式的创建

--- summarize/JSDomeTest/闭包-封装变量.js
+++ summarize/JSDomeTest/闭包-封装变量.js
@@ -1,7 +1,7 @@
 var mult = (function () {
   var cache = {}
   return function () {
-    var arg = Array.prototype.join.call(arguments);
+    var arg = Array.prototype.join.call(arguments , ',');
     if ( cache[arg]) {
       return cache[arg]
     }
@@ -10,5 +10,24 @@ var mult = (function () {
       a = a * arg[i]
     }
     return cache[arg] = a;
+  }
+})()
+
+var mult = (function () {
+  var cache = {}
+  var calculate = function () {
+    var a = 1;
+    for (var i = 1 ; i < arguments.length ; i ++) {
+      a = a * arguments[i]
+    }
+    return a
+  }
+
+  return function () {
+    var args = Array.prototype.join.call(arguments , ',');
+    if ( cache[args] ) {
+      return cache[args]
+    }
+    return cache[args] = calculate.apply( null , arguments);
   }
 })()
