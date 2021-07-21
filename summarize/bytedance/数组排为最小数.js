c49d8821d9ffb77c4762ebfc1f635dd33d096c57
OrangeXu <1249858341@qq.com>
2021-07-21 17:34:02 +0800

    update

---
+++ summarize/bytedance/数组排为最小数.js
@@ -0,0 +1,17 @@
+var pailie = function (arr) {
+  var Arr = []
+  arr.forEach(element => {
+    Arr.push(element.toString())
+  });
+  var kuaipai = function (Arr) {
+    let index = Math.floor(Arr.length/2);
+    let indexValue = Arr[index];
+    let left = [];
+    let right = [];
+    for ( var i = 0 ; i < Arr.length ; i ++) {
+
+    }
+  }
+}
+var arr = [3,30,34,5,9]
+console.log(pailie(arr))
--- summarize/bytedance/最大子序和.js
+++ summarize/bytedance/最大子序和.js
@@ -1,37 +1,33 @@
 const arr = [1,-3,4, -1, 1, 2, -5]
 function getMax (arr) {
     var sum = 0;
-    var max = 0;
+    var max = arr[i];
+    var first = 0;
+    var end = 0;
     for (var i = 0 ; i < arr.length ; i ++){
         if (sum < 0){
             sum = arr[i];
+            first = i
         }else{
             sum += arr[i]
         }
-        max = Math.max(max,sum);
+        if ( max < sum) {
+          max = sum;
+          end = i
+        }
     }
-    return max;
-}
-// function getMax(arr) {
-//     const memo = []
-//     memo[0] = arr[0]
-//     let max = arr[0]
-//     for(let i = 1;i<arr.length;i++) {
-//         memo[i] = Math.max(arr[i] + memo[i  -1], arr[i])
-//         max = Math.max(max, memo[i])
-//     }
-//     return max
-// }
+    return `${first} -- ${end} : ${max}`;
+  }
 
 console.log(getMax(arr))
 
-function getMax(arr){
-    var max = arr[0];
-    var memo = [];
-    memo[0] = arr[0];
-    for (var i = 1 ; i < arr.length ; i ++){
-        memo[i] = Math.max(memo[i - 1] + arr[i], arr[i]);
-        max = Math.max(memo[i],max);
-    }
-    return max;
-}
+// function getMax(arr){
+//     var max = arr[0];
+//     var memo = [];
+//     memo[0] = arr[0];
+//     for (var i = 1 ; i < arr.length ; i ++){
+//         memo[i] = Math.max(memo[i - 1] + arr[i], arr[i]);
+//         max = Math.max(memo[i],max);
+//     }
+//     return max;
+// }
